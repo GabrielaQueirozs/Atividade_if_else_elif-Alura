93a2d83654d47add87ff012eb663734a71d5366a
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-09-16 11:01:48 -0300

    Temperatura da sala de servidores

---
+++ temperatura_sala.py
@@ -0,0 +1,5 @@
+temperatura = float(input("Digite a temperatura atual:"))
+if temperatura > 25:
+    print("ALERTA! a temperatura está acima do permitido")
+elif temperatura <= 25:
+    print("A temperatura está dentro do permitido")
