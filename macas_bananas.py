62f00cc46d571ebd9a03f42f07021cc2d529f42d
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-09-16 10:26:31 -0300

    Atividade if-else-elif

---
+++ macas_bananas.py
@@ -0,0 +1,8 @@
+macas = int(input("Digite a quantidade de maças vendidas:"))
+bananas = int(input("Digite a quantidade de bananas vendidas:"))
+if macas > bananas :
+    print("Aa maças venderam mais que as bananas.")
+elif bananas > macas :
+     print("As bananas venderam mais que as maças") 
+else :
+    print("As bananas e as maças tiveram o mesmo total de vendas")
