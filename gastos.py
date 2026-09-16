f3e5efc97db4cc734fd934d15c818227d809914a
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-09-16 13:17:05 -0300

    despesas do mês

---
+++ gastos.py
@@ -0,0 +1,6 @@
+despesas = float(input("Digite o total de despesas dessse mês: R$"))
+if despesas <= 3.000:
+    print("ARRASOU! você esta dentro do orçamento")
+elif despesas > 3.000:
+    print("ATENÇÃO! você ultrapassou o limite do orçamento :(")    
+
